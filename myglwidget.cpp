4c58c1d431d168cdade3075a4eca81f265a20e85
pandacase <2227119810@qq.com>
2024-01-24 21:23:27 +0800

    Update myglwidget.cpp

--- myglwidget.cpp
+++ myglwidget.cpp
@@ -92,7 +92,7 @@ void MyGLWidget::initializeGL() {
 }
 
 void MyGLWidget::paintGL() {
-  showFullScreen();
+  // showFullScreen();
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glLoadIdentity();
   glBindTexture(GL_TEXTURE_2D, m_Texture);
