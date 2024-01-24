8e8576904cc1f5c76394a875a35a3b2d6587dab6
pandacase <2227119810@qq.com>
2024-01-24 14:48:24 +0800

    update comments

--- myglwidget.h
+++ myglwidget.h
@@ -14,9 +14,8 @@
 #include <QGLWidget>
 #include <QTimer>
 
-class MyGLWidget: public QGLWidget
-{
-     Q_OBJECT
+class MyGLWidget: public QGLWidget {
+    Q_OBJECT
 
 public:
     explicit MyGLWidget(QWidget * parent = 0);
@@ -24,36 +23,47 @@ public:
 
 protected:
     void initializeGL();
+
+    //! @brief Handle window size adjustment.
+    //! 
+    //! @param w : New width
+    //! @param h : New height
     void resizeGL(int w, int h);
+    
+    //! @brief Paint the scene
+    //! 
     void paintGL();
-    void keyPressEvent(QKeyEvent * e);              //处理键盘按下事件
+
+    //! @brief Handle key press event
+    //! 
+    //! @param e : the pressed key
+    void keyPressEvent(QKeyEvent * e);              //!< Key Press Event
 
 private:
-    bool fullscreen;                                //是否全屏显示
-    QString m_FileName;                             //图片的路径及文件名
-    GLuint m_Texture;                               //储存一个纹理
+    bool fullscreen;                                //!< [true] -> is full screen
+    QString m_FileName;                             //!< particle texture path
+    GLuint m_Texture;                               //!< particle texture
 
-    static const int MAX_PARTICLES = 1000;          //最大粒子数
-    static const GLfloat COLORS[12][3];             //彩虹的颜色
-    bool m_Rainbow;                                 //是否为彩虹模式
-    GLuint m_Color;                                 //当前的颜色
+    static const int MAX_PARTICLES = 1000;          //!< maximum number of particles
+    static const GLfloat COLORS[12][3];             //!< rainbow color
+    bool m_Rainbow;                                 //!< [true] -> rainbow mode (particle in rainbow color)
+    GLuint m_Color;                                 //!< current color
 
-    float m_Slowdown;                               //减速粒子
-    float m_xSpeed;                                 //x方向的速度
-    float m_ySpeed;                                 //y方向的速度
-    float m_Deep;                                   //移入屏幕的距离
+    float m_Slowdown;                               //!< deceleration coefficient
+    float m_xSpeed;                                 //!< x-axis velocity
+    float m_ySpeed;                                 //!< y-axis velocity
+    float m_Deep;                                   //!< deepth
 
-    struct Particle                                 //创建粒子结构体
-    {
-        bool active;                                //是否激活
-        float life;                                 //粒子生命
-        float fade;                                 //衰减速度
+    struct Particle {
+        bool active;                                //!< [true] -> is active, show on screen
+        float life;                                 //!< the life span of a particle
+        float fade;                                 //!< fade velocity
 
-        float r, g, b;                              //粒子颜色
-        float x, y, z;                              //位置坐标
-        float xi, yi, zi;                           //各方向速度
-        float xg, yg, zg;                           //各方向加速度
-    } m_Particles[MAX_PARTICLES+1];                 //存放1001个粒子的数组,最后一个粒子设置为触发器粒子
+        float r, g, b;                              //!< color
+        float x, y, z;                              //!< postion
+        float xi, yi, zi;                           //!< velocity
+        float xg, yg, zg;                           //!< gravity
+    } m_Particles[MAX_PARTICLES+1];                 //!< max-num particle with ONE trigger
 };
 
 #endif // MYGLWIDGET_H
